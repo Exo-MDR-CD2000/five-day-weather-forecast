21170a18cbadab0f977c8c832e854ea4f0f6230f
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-07-24 16:41:59 -0700

    more api notes

--- assets/js/script-main.js
+++ assets/js/script-main.js
@@ -100,5 +100,6 @@ fetch(urlForecast)
 
 //the five day forecast api does have data for the city name, date, temperature, wind speed, and humidity
 //Had to ctrl + f on the docs to find it. It's under the "city" object.
+//No need to use lat or lon! It's strange that the example api url's dont show that you can use the ?q=city parameter
 
 
